5e54e9c907f30a188566bc50dfe6db164bf6b578
Sam Aaron <samaaron@gmail.com>
2011-09-24 00:11:43 +0200

    Depend on Clojure 1.3.0 final

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject serial-port "1.1.0"
+(defproject serial-port "1.1.1"
   :description "Simple serial port comms library. Wraps RxTx."
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [rxtx22 "1.0.5"]])

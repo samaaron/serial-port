6141ab8f356a53db6c5f3ab0ea673fa175882c1d
Sam Aaron <samaaron@gmail.com>
2012-03-23 12:23:52 +0000

    rely on rxtx22 1.0.6 which is compatible with lein 2.x

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject serial-port "1.1.1"
+(defproject serial-port "1.1.2"
   :description "Simple serial port comms library. Wraps RxTx."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [rxtx22 "1.0.5"]])
+                 [rxtx22 "1.0.6"]])
